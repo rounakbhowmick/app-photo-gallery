9ae0db5eee71eb3d5cbfb4d44d0eea7450e968ee
Rounak Bhowmick <rounakchakda@gmail.com>
2020-07-15 13:31:47 +0530

    FSJ0620A108

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule
